44d8b2710fddf163c54d1fd36f991299d1a0eb71
Syed Ghazanfar Abbas Zaidi <160645162+syedghazanfar110@users.noreply.github.com>
2024-04-07 00:36:35 +0500

    online class 8 project currency converter

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let currency = {
+    USD: 1,
+    EUR: 0.9,
+    IND: 75,
+    PKR: 200,
+};
+let user_answer = await inquirer.prompt([{
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "IND", "PKR"]
+    },
+    {
+        name: "to",
+        message: "Enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "IND", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter amount",
+        type: "number",
+    },
+]);
+let amount = user_answer.amount;
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let totalAmount = amount / fromAmount;
+let total = totalAmount * toAmount;
+console.log(total);
